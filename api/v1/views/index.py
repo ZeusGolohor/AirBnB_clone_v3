594704bccd0643500ddb9a5469cac0a17886e63b
golohorzeus <golohorzeusufuoma@gmail.com>
2024-02-25 20:21:50 -0800

    Create an endpoint that retrieves the number of each objects by type:
    In api/v1/views/index.py
    Route: /api/v1/stats
    You must use the newly added count() method from storage

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,3 +12,27 @@ def status():
     status.
     """    
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def stat():
+    """
+    A method to get the statistics for all models
+    from the specified storage system.
+    """
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    from models import storage
+
+    classes = {"Amenity": Amenity, "City": City,
+                "Place": Place, "Review": Review,
+                 "State": State, "User": User}
+    
+    stats = {}
+    for key, cls in classes.items():
+        stats[key] = storage.count(cls)
+    return jsonify(stats)
+
